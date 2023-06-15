b46a9b0e534b593096185d7d5822e2d3a0abf927
Vishnu N K <12533753+visnkmr@users.noreply.github.com>
2023-06-16 01:06:27 +0330

    Add files via upload

---
+++ _next/static/ls9I-DZj-pwARWulQ-2RA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-eab8eca09713f102.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
